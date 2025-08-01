f63157cf80a47ddf8dee0c858e893ac8a485b406
Alireza-MH <alireza.mehdizadeh.84.313@gmail.com>
2025-08-01 13:45:24 +0330

    The robot is ready for the first stage.

---
+++ src/utils/openai.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai"
+
+const rules: string[] = [
+  "اسم تو نویسا هست، هوش مصنوعی ویرایش متن و هیچ اسم دیگری نداری و اگر کسی اسمت رو نپرسید نگو یکسره بهش",
+  "سازنده تو علیرضا مهدی زاده هست",
+  "تو میتونی متنی که بهت مبدن رو ویرایش کنی یا کم و زیاد کنی بر اساس دستوری که کاربر میده اگر بخواد و اگر چیزی نیاز نداشت کاری نمیکنی ازش میپرسی باید چیکار کنی",
+  "هیچ سوالی که مربوط به متن نیست رو جواب نده",
+]
+
+const openai = new OpenAI({
+  baseURL: "https://ai.liara.ir/api/v1/6839ae390174d5611eec5839",
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const askAI = async (messages: string[]) => {
+  const userMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = messages.map(
+    (content) => ({
+      role: "user",
+      content,
+    })
+  )
+
+  const completion = await openai.chat.completions.create({
+    model: "google/gemini-2.0-flash-001",
+    messages: [
+      {
+        role: "system",
+        content: `سلام، قوانین زیر رو باید در هرصورت رعایت کنید\n\n ${rules
+          .map((rule, index) => `${index + 1}. ${rule}`)
+          .join("\n")}`,
+      },
+
+      ...userMessages,
+    ],
+  })
+
+  return completion.choices[0].message
+}
+
+export default askAI
